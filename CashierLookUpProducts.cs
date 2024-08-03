cc6949075356a453822a25acd8f7c218951287e4
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-08-03 23:55:52 +0500

    feat: lookup products for cashier module (#46) issues #43 #44 #45
    - Create search product form
    - Create cart table in database
    - Adding product to table cart with trigger
    - Populate Data from Sql Server To DataGridView
    - Display cart
    - Calculate Total Sales
    - Adding quantity stock in module
    - Added some assets
    - Created quantity module form

---
+++ CashierLookUpProducts.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSales
+{
+    public partial class CashierLookUpProducts : Form
+    {
+        /// HANDLE THE CONNECTION TO DATABASE
+        SqlConnection connection = new SqlConnection();
+
+        /// TO EXECUTE SQL COMMANDS AND QUERIES
+        SqlCommand sqlCommand = new SqlCommand();
+
+        /// CUSTOM CLASS FOR MANAGING DATABASE CONNECTIONS
+        DatabaseConnectionClass connectionClass = new DatabaseConnectionClass();
+
+        /// TO READ DATA RETRIEVED FROM DATABASE
+        SqlDataReader dataReader;
+
+        /// CASHIER FORM
+        CashierForm cashierForm;
+
+        public CashierLookUpProducts(CashierForm cashierForm)
+        {
+            InitializeComponent();
+
+            // Establish Connection
+            connection = new SqlConnection(connectionClass.DatabaseConnection());
+
+            // Load Categories to DataGridView
+            loadProducts();
+
+            // Assign Received cashierForm Argument to Global Variable
+            this.cashierForm = cashierForm;
+
+            loadProducts();
+        }
+
+        /// DISPLAY DATA RETREIVE FROM tbProduct to dgvProduct
+        public void loadProducts()
+        {
+            // To Keep Track of Row Number
+            int i = 0;
+
+            // Clear All Rows from DataGridView to Prepare for Fresh Data
+            dgvProduct.Rows.Clear();
+
+            // Open Database Connection
+            connection.Open();
+
+            // SQL Command to Select All Records from tbProduct Table, Also for Searching
+            // sqlCommand = new SqlCommand("SELECT p.productCode, p.barcode, p.description, b.brand, c.category, p.price, p.quantity, p.reorder, p.weight from tbProduct AS p INNER JOIN tbBrand as b ON b.id = p.brandId INNER JOIN tbCategory AS c on c.id = p.categoryId WHERE CONCAT(p.description, b.brand, c.category) LIKE '%" +txtSearchProduct.Text + "%'", connection);
+
+            // Search By ProductCode, Barcode, Category, Brand, Also for Loading Products
+            sqlCommand = new SqlCommand("SELECT p.productCode, p.barcode, p.description, b.brand, c.category, p.price, p.quantity, p.reorder, p.weight from tbProduct AS p INNER JOIN tbBrand as b ON b.id = p.brandId INNER JOIN tbCategory AS c on c.id = p.categoryId WHERE p.productCode LIKE '%" + txtSearchProduct.Text + "%' OR p.barcode LIKE '%" + txtSearchProduct.Text + "%' OR b.brand LIKE '%" + txtSearchProduct.Text + "%' OR c.category LIKE '%" + txtSearchProduct.Text + "%'", connection);
+
+            // Execute SQL Command, Obtain SQLDataReader to Read Data from Database
+            dataReader = sqlCommand.ExecuteReader();
+
+            // Iterate through the DataReader to Read Each Row of Data
+            while (dataReader.Read())
+            {
+                // Increment Counter for Each Row
+                i++;
+
+                // Add New Row to DataGridView With Counter, Id, Brand Values from the Current Row
+                dgvProduct.Rows.Add(i, dataReader[0].ToString(), dataReader[1].ToString(), dataReader[2].ToString(), dataReader[3].ToString(), dataReader[4].ToString(), dataReader[5].ToString(), dataReader[6].ToString(), dataReader[7].ToString(), dataReader[8].ToString());
+            }
+
+            // Close DataReader After Reading All Data
+            dataReader.Close();
+
+            // Close Database Connection
+            connection.Close();
+        }
+
+        /// CLOSE WINDOW
+        private void picClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void dgvProduct_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Add Product By Cell Click from in tbCart
+            string databaseOperation = dgvProduct.Columns[e.ColumnIndex].Name;
+
+            if (databaseOperation == "AddToCart")
+            {
+                // To Add Product Name in Cart Table
+                QuantityModule quantityModule = new QuantityModule(cashierForm);
+
+                quantityModule.productDetails(dgvProduct.Rows[e.RowIndex].Cells[1].Value.ToString(), double.Parse(dgvProduct.Rows[e.RowIndex].Cells[6].Value.ToString()), cashierForm.lblTransactionNumberActual.Text, int.Parse(dgvProduct.Rows[e.RowIndex].Cells[7].Value.ToString()));
+
+                quantityModule.ShowDialog();
+            }
+            // Load Products
+            loadProducts();
+        }
+
+        private void txtSearchProduct_TextChanged(object sender, EventArgs e)
+        {
+            loadProducts();
+        }
+
+        private void picClose_Click_1(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
---
+++ QuantityModule.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace POSales
+{
+    partial class QuantityModule
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QuantityModule));
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.picClose = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantity.Location = new System.Drawing.Point(12, 64);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(258, 30);
+            this.txtQuantity.TabIndex = 4;
+            this.txtQuantity.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtQuantity.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtQuantity_KeyPress);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+            this.panel1.Controls.Add(this.picClose);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 45);
+            this.panel1.TabIndex = 5;
+            // 
+            // picClose
+            // 
+            this.picClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.picClose.Image = ((System.Drawing.Image)(resources.GetObject("picClose.Image")));
+            this.picClose.Location = new System.Drawing.Point(240, 5);
+            this.picClose.Margin = new System.Windows.Forms.Padding(0);
+            this.picClose.Name = "picClose";
+            this.picClose.Size = new System.Drawing.Size(30, 30);
+            this.picClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picClose.TabIndex = 4;
+            this.picClose.TabStop = false;
+            this.picClose.Click += new System.EventHandler(this.picClose_Click);
+            // 
+            // QuantityModule
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(280, 110);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtQuantity);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(280, 100);
+            this.Name = "QuantityModule";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QuantityModule";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picClose;
+    }
+}
