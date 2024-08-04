04d2b6b52757040bda74730e3d74582b31fda1a2
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-08-04 18:19:52 +0500

    feat: product stock module
    - Created and designed product stock module
    - Featched prodcuts from tbProduct and displayed in data grid view

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace POSales
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new StockInModule());
+            Application.Run(new ProductStockIn());
         }
     }
 }
