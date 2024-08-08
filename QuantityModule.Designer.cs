b5f3e704fc1d39a748ba392a7e23f5d54c30c01f
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-08-08 16:56:00 +0500

    quantity form modifications (#75) issues #73 #74
    - Changed the close icon
    - Added disposing form on escape key

--- QuantityModule.Designer.cs
+++ QuantityModule.Designer.cs
@@ -32,6 +32,7 @@
             this.txtQuantity = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.picClose = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picClose)).BeginInit();
             this.SuspendLayout();
@@ -39,7 +40,7 @@
             // txtQuantity
             // 
             this.txtQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtQuantity.Location = new System.Drawing.Point(12, 64);
+            this.txtQuantity.Location = new System.Drawing.Point(130, 64);
             this.txtQuantity.Name = "txtQuantity";
             this.txtQuantity.Size = new System.Drawing.Size(258, 30);
             this.txtQuantity.TabIndex = 4;
@@ -52,29 +53,41 @@
             this.panel1.Controls.Add(this.picClose);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(280, 45);
+            this.panel1.Size = new System.Drawing.Size(400, 35);
             this.panel1.TabIndex = 5;
             // 
             // picClose
             // 
             this.picClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.picClose.Image = ((System.Drawing.Image)(resources.GetObject("picClose.Image")));
-            this.picClose.Location = new System.Drawing.Point(240, 5);
+            this.picClose.Location = new System.Drawing.Point(364, 3);
             this.picClose.Margin = new System.Windows.Forms.Padding(0);
             this.picClose.Name = "picClose";
-            this.picClose.Size = new System.Drawing.Size(30, 30);
+            this.picClose.Size = new System.Drawing.Size(25, 25);
             this.picClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.picClose.TabIndex = 4;
             this.picClose.TabStop = false;
             this.picClose.Click += new System.EventHandler(this.picClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Quantity";
             // 
             // QuantityModule
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(280, 110);
+            this.ClientSize = new System.Drawing.Size(400, 110);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.txtQuantity);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -83,6 +96,7 @@
             this.Name = "QuantityModule";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "QuantityModule";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.QuantityModule_KeyDown);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picClose)).EndInit();
@@ -94,6 +108,7 @@
         #endregion
         private System.Windows.Forms.TextBox txtQuantity;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox picClose;
     }
 }
